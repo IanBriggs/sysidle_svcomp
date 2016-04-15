867dec0b91860575944e85d6d087af1a297db7a5
Ian Briggs <ian.briggs@utah.edu>
2016-04-15 14:44:59 -0600

    removed my copied code

--- svcomp_include/cmpxchg_32_sat.h
+++
@@ -1,4 +0,0 @@
-#ifndef _ASM_X86_CMPXCHG_32_H
-#define _ASM_X86_CMPXCHG_32_H
-
-#endif /* _ASM_X86_CMPXCHG_32_H */
--- svcomp_include/svcomp_replacements.c
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-
-void __VERIFIER_error() {
-  assert(0);
-}
-
-void __VERIFIER_atomic_begin(){};
-void __VERIFIER_atomic_end(){};
